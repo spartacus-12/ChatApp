aa53ca10595ee35771038530b5f750f069d42c4a
Ghassen <ghassen.zmerli@sesame.com.tn>
2018-11-11 20:46:29 +0100

    chatApp

---
+++ src/main/java/org/sesame/SpringChatAppApplication.java
@@ -0,0 +1,12 @@
+package org.sesame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringChatAppApplication.class, args);
+	}
+}
